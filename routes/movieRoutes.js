86fa3cea70ecc1cc1f02dc6a300cb23e898326c9
Sintja <sporeci2000@gmail.com>
2025-08-20 14:01:23 -0400

    Router

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
+
+// Importing controller functions
 const { searchMovies, getMovieDetails } = require('../controllers/movieController');
 
+// Create a router instance
 const router = express.Router();
 
+// Connect it to the searchMovies controller function
 router.get('/search', searchMovies);
 
+// Connect it to the getMovieDetails controller function
 router.get('/movies/:id', getMovieDetails);
 
+// Export the router so it can be mounted in server.js
 module.exports = router;
